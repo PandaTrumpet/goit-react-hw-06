fbdd62cf072c764958440ec7a4f0eeb8fc6e8cfc
PandaTrumpet <dsavontrumpet@gmail.com>
2024-05-07 12:41:52 +0300

    update

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,17 +2,21 @@ import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 export default function ContactList() {
   const selectContacts = useSelector((state) => state.contacts.items);
+  const selectFilter = useSelector((state) => state.filters.name);
+  // console.log(selectFilter);
 
   return (
     <div>
       <ul>
-        {selectContacts.map((contact) => {
-          return (
+        {selectContacts
+          .filter((contact) =>
+            contact.name.toLowerCase().includes(selectFilter.toLowerCase())
+          )
+          .map((contact) => (
             <li key={contact.id}>
               <Contact contact={contact} />
             </li>
-          );
-        })}
+          ))}
       </ul>
     </div>
   );
--- src/redux/filtersSlice.js
+++ src/redux/filtersSlice.js
@@ -3,15 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const filterSlice = createSlice({
   name: "filter",
   initialState: {
-    items: [],
     name: "",
   },
   reducers: {
     searchContact: {
       reducer(state, action) {
-        state.items = state.items.filter((contact) =>
-          contact.name.includes(action.payload)
-        );
+        state.name = action.payload;
       },
     },
   },
